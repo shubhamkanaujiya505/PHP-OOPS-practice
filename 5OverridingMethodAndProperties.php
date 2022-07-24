29ba71042f8a19cf30f4907a247fcd078ae19375
Shubham Kanaujiya <shubhamkanaujiya505@gmail.com>
2022-07-25 01:54:08 +0530

    5 method overriding

--- 5OverridingMethodAndProperties.php
+++ 5OverridingMethodAndProperties.php
@@ -1,14 +1,43 @@
 <?php
+// class base{
+//     public $name = "shubham kanaujiya";
+
+// }
+// class drived extends base{
+//     public $name = "kanaujiya shubham";
+// }
+// $object = new base();
+// $object1 = new drived();
+// echo $object->name."<br>";
+// echo $object1->name;
+
+?>
+
+
+
+
+<?php
+
+// method overridings
 class base{
     public $name = "shubham kanaujiya";
+    public function cal($a, $b){
+        return $a * $b;
+    }
 
 }
 class drived extends base{
     public $name = "kanaujiya shubham";
+    public function cal($a, $b){
+        return $a + $b;
+    }
 }
-$object = new base();
+// $object = new base();
+
 $object1 = new drived();
-echo $object->name."<br>";
-echo $object1->name;
+
+// echo $object->cal(5,8);
+
+echo $object1->cal(5,8);
 
 ?>
