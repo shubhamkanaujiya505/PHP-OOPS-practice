b199847298ef16b4b4a46dd677e338db9d6baf48
ginger <shubham Kanaujiya>
2022-07-25 17:23:49 +0530

    14Method_Chaining

---
+++ 14Method_Chaining.php
@@ -0,0 +1,21 @@
+<?php
+class abc{
+    public function first(){
+        echo "this is first function"."<br>";
+        return $this;
+    }
+    public function second(){
+        echo "this is second function"."<br>";
+        return $this;
+    }
+    public function third(){
+        echo "this is third function\n";
+    }
+}
+$obj = new abc();
+
+$obj->first()->second()->third();
+// $obj->first();
+// $obj->second();
+// $obj->third();
+?>
--- namespace/products.php
+++ namespace/products.php
@@ -1,5 +1,5 @@
 <?php
-namespace pro;
+namespace pro\v1\command;
 class product{
     public function __construct(){
         // echo "This is product class <br>";
