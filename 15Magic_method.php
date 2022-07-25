cf682061e1a0f901d8e112431d73d8e8e580078f
ginger <shubham Kanaujiya>
2022-07-25 17:34:24 +0530

    15Magic_method

---
+++ 15Magic_method.php
@@ -0,0 +1,22 @@
+<?php
+// automatically run based on condition
+// __construct();
+// they start with prifix __ function
+
+// __construct()
+// __destruct()
+// __call($fun, $arg)
+// __callStatic($fun, $arg)
+// __get($property)
+// __set($property, $value)
+// __isset($content)
+// __unset($content)
+// __sleep()
+// __wakeup()
+// __toString()
+// __invoke()
+// __set_state($array)
+// __clone()
+// __debugInfo()
+
+?>
